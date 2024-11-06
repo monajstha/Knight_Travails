bafc060830fe12bae610df481dc23bcc915a7289
monajstha <manojstha700@gmail.com>
2024-11-06 06:13:25 +0545

    add: add possible moves of a knight

---
+++ index.js
@@ -0,0 +1,13 @@
+function shortestKnightMove(start, end) {
+  // possible moves of a Knight
+  const possibleMoves = [
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+    [1, 2],
+    [-1, 2],
+    [1, -2],
+    [-1, -2],
+  ];
+}
